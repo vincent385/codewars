820fa8232592a45034c12f40fc577eaa4a7968fa
vincent385 <vincentbell103@outlook.com>
2022-12-31 17:09:53 +0000

    solved "Find the odd int"

---
+++ 6 kyu/Find the odd int.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// From an array of integers, find the one that appears an odd number of times.
+int find_odd (size_t length, const int array[length])
+{
+    for (int i = 0; i < length; i++) {
+        int count = 0;
+        for (int j = 0; j < length; j++) {
+            if (array[i] == array[j])
+                count++;
+        }
+        if (count % 2 != 0)
+            return array[i];
+    }
+}
