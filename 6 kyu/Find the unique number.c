6128bb2a321cc2f9b69b07edabd9c067a8f3d5c6
vincent385 <vincentbell103@outlook.com>
2023-01-06 23:05:03 +0000

    📝 solved "Find the unique number" C version

--- 6 kyu/Find the unique number.c
+++ 6 kyu/Find the unique number.c
@@ -1,28 +1,16 @@
-#include <stdio.h>
 #include <stddef.h>
 
 float finduniq(const float *nums, size_t n)
 {
-    size_t i = 0;
-    size_t j = 0;
-    int count = 0;
-    while (i < n) {
-        if (nums[i] == nums[j])
-            count++;
-        if (j % n == 0 && j != 0) {
-            if (count == 1)
-                break;
-            i++;
-            j = 0;
-            count = 0;
+    if (nums[0] != nums[1])
+        if (nums[0] != nums[2])
+            return nums[0];
+        else
+            return nums[1];
+    else {
+        for (size_t i = 2; i < n; i++) {
+            if (nums[i] != nums[0])
+                return nums[i];
         }
-        j++;
     }
-    return nums[i];
-}
-
-int main () {
-    const float f[6] = {1, 1, 1, 2, 1, 1};
-    printf("unique: %f\n", finduniq(f, 6));
-    return 0;
 }
